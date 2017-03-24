153b77f45a1d97179161325adb78b97b9afa75cc
Jesse OdB <jesse.opdebeeck@student.kdg.be>
2017-03-24 18:06:54 +0100

    reset alles tot toevoegen frontend workflow

--- routes/web.php
+++ routes/web.php
@@ -12,49 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
-
-// redirect /campi naar lijst van campi
-Route::get('campi', function() {
-	return view('campi');
-});
-
-// redirect /dashboard naar admin dashboard
-Route::get('dashboard', function() {
-	return view('admin-dashboard');
-});
-
-// redirect /bezienswaardigheden naar overzicht bezienswaardigheden
-Route::get('bezienswaardigheden', function() {
-	return view('views');
-});
-
-// redirect /statistieken naar statistieken pagina
-Route::get('statistieken', function() {
-	return view('gamestats');
-});
-
-// redirect /links naar pagina met interessante links
-Route::get('links', function() {
-	return view('links');
-});
-
-// redirect /nieuws naar nieuwspagina
-Route::get('nieuws', function() {
-	return view('news');
-});
-
-// redirect /getuigenissen naar testimonials pagina
-Route::get('getuigenissen', function() {
-	return view('testimonials');
-});
-
-// redirect /profiel naar userprofile
-Route::get('profiel', function() {
-	return view('userprofile');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
