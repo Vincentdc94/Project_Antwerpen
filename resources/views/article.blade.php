605bba68e360ff92299c08dd80af356dbb791d30
vincentdc94 <vincent.decoen94@gmail.com>
2017-04-24 20:29:28 +0200

    key rariteiten temp fix

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -6,7 +6,7 @@
 
 @section("content")
 <div class="container">
-  <div class="article">  
+  <div class="article">
     <h1 class="article-title">{{ 'Korting voor studenten van het mas' }}</h1 class="article-title">
 
     <div class="article-media">
