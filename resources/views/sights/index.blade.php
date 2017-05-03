b425bb7d3249313be5b61cbfc86878d9085af491
jessekdg <spificator@hotmail.com>
2017-05-03 19:41:45 +0200

    bezienswaardigheden index, foreign key naar contact moet nog gedaan worden

--- resources/views/sights/index.blade.php
+++ resources/views/sights/index.blade.php
@@ -5,5 +5,17 @@
 </head>
 <body>
 
+	<h1>Bezienswaardigheden</h1>
+	<ul>
+	@foreach($sights as $sight)
+	<li>
+		Naam: {{ $sight->name }}<br>
+		Beschrijving: {{ $sight->description }}<br>
+		Contact: {{ $sight->contact_id }}	
+		<hr>
+	</li>
+	@endforeach
+	</ul>
+
 </body>
 </html>
