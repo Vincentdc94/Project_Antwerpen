4a9d14623154454502d771dd6ee7ff8097ef9333
jessekdg <spificator@hotmail.com>
2017-05-07 21:48:11 +0200

    links algemene backend af

--- resources/views/links/index.blade.php
+++ resources/views/links/index.blade.php
@@ -1,19 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>links</title>
-</head>
-<body>
+@extends('layouts.app')
 
-	<h1>Links index</h1>
-	<ul>
-	@foreach($links as $link)
-		<li>
-			<b>Naam:</b> {{ $link->name }}<br>
-			<b>Url:</b> {{ $link->url }}
-		</li>
-	@endforeach
-	</ul>
+@section("header")
+  @include('partials.header', array('title' => "Links", 'menu' => false))
+@endsection
 
-</body>
-</html>
+@section("content")
+  <div class="container">
+		<h4>Interessante links</h4>
+		<div class="table-holder">
+			<table>
+			<thead>
+				<tr>
+					<th>Naam</th>
+                    <th>Link</th>
+                    <th>Beschrijving</th>
+				</tr>
+			</thead>  
+			<tbody>
+				@foreach($links as $link)
+					<tr>
+						<td>{{ $link->name }}</td>
+                        <td><a href="{{ $link->url }}">{{ $link->url }}</a></td>
+                        <td>{{ $link->description }}</td>
+					</tr>
+				@endforeach
+			</form>
+			</tbody>
+		</table>
+	</div>
+
+  </div>
+@endsection
