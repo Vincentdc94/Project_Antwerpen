2024661224b749e21aaa9d434b7bdb39e45df2d1
jessekdg <spificator@hotmail.com>
2017-05-07 16:58:58 +0200

    password reset knop werkt, link versturen nog niet

--- routes/web.php
+++ routes/web.php
@@ -101,8 +101,23 @@ Route::get('admin/links/{id}/bewerken', 'LinkController@edit');
 Route::patch('/links/{id}', 'LinkController@update');
 Route::delete('admin/links/{id}', 'LinkController@destroy');
 
-/* * LARAVEL DEFAULT PASSWORD RESET ROUTES * */
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+
+
+
+
+
+/* * GEGENEREERD DOOR Auth::routes(); * */
+/* Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');*/
+
+/* Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');*/
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
