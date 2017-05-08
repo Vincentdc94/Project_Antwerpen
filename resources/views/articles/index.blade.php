595f478f5c5e4e25c8af4f1593f98d200509b292
Vincent De Coen <contact@vincentdecoen.be>
2017-05-08 14:15:12 +0200

    nieuws artikels pagina

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,116 +1,82 @@
-@extends('layouts.app')
+@extends('layouts.app') 
 
-@section('header')
-    @include('partials.header-titled', array('title' => 'Nieuws'))
-@endsection
-
+@section('header') 
+    @include('partials.header-titled', array('title' => 'Nieuws')) 
+@endsection 
 
 @section("content")
 
 <div class="container">
-
-<div class="row">
-
-@foreach($articles as $article)
-<a href="/artikels/{{ $article->id }}">
-<div class="col-perc-60-gt-30">
-            <div class="news-item box-large">
-                <img src="{{ asset('images/hero.jpg') }}" class="news-image" alt="Hero image">
-                <div class="news-overlay">
-                    <div class="news-category">{{ $article->category->name }}</div>
-                    <div class="news-title">
-                        <h3>{{ $article->title }}</h3>
-                    </div>
-                        <div class="news-button">Lees Artikel</div>
-                    <div class="news-details">
-                        <div class="news-author">{{ $article->author->firstName }} {{ $article->author->lastName }}</div>
-                        <div class="news-date">{{ $article->created_at->diffForHumans() }}</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-</a>
-@endforeach
-
-    {{--
+    <div class="row">
         <div class="col-perc-60-gt-30">
+            <a href="{{ url('/artikels/' . $articles[0]->id) }}">
             <div class="news-item box-large">
                 <img src="{{ asset('images/hero.jpg') }}" class="news-image" alt="Hero image">
                 <div class="news-overlay">
-                    <div class="news-category">{{ $article->category->name }}</div>
+                    <div class="news-category">{{ $articles[0]->category->name }}</div>
                     <div class="news-title">
-                        <h3>{{ $article->title }}</h3>
+                        <h3>{{ $articles[0]->title }}</h3>
                     </div>
                     <div class="news-button">Lees Artikel</div>
                     <div class="news-details">
-                        <div class="news-author">{{ $article->author->firstName }} {{ $article->author->lastName }}</div>
-                        <div class="news-date">{{ $article->created_at->diffForHumans() }}</div>
+                        <div class="news-author">{{ $articles[0]->author->firstName }} {{ $articles[0]->author->lastName }}</div>
+                        <div class="news-date">{{ $articles[0]->created_at->diffForHumans() }}</div>
                     </div>
                 </div>
             </div>
-        </div>
+            </a>
 
-
-
-
-    <div class="col-perc-60-gt-30">
-        <div class="news-item box-large">
-            <img src="{{ asset('images/hero.jpg') }}" class="news-image" alt="Hero image">
-            <div class="news-overlay">
-                <div class="news-category">{{ $articles[0]->category->name }}</div>
-                <div class="news-title">
-                    <h3>{{ $articles[0]->title }}</h3>
-                </div>
-                <div class="news-button">Lees Artikel</div>
-                <div class="news-details">
-                    <div class="news-author">{{ $articles[0]->author->firstName }} {{ $articles[0]->author->lastName }}</div>
-                    <div class="news-date">{{ $articles[0]->created_at->diffForHumans() }}</div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-perc-60-gt-30">
-        <div class="row" style="width: 102%;">
-            @for($articleIndex = 0; $articleIndex < count($articles); $articleIndex)
-                @if($articleIndex & 1)
-                    <div class="col-2-gt-30">
-                    <div class="news-item box-medium">
-                    <img src="{{ asset('images/hero.jpg') }}" class="news-image" alt="Hero image">
-                    <div class="news-overlay">
-                        <div class="news-category">{{ $articles[$articleIndex]->category->name }}</div>
-                        <div class="news-title">
-                            <h3>{{ $articles[$articleIndex]->title }}</h3>
+            <div class="row" style="width: 102%;">
+                @for($articleIndex = 1; $articleIndex < count($articles); $articleIndex++) 
+                    @if($articleIndex & 1) 
+                        <div class="col-2-gt-30">
+                            <a href="{{ url('/artikels/' . $articles[$articleIndex]->id) }}">
+                            <div class="news-item box-medium">
+                                <img src="{{ asset('images/hero.jpg') }}" class="news-image" alt="Hero image">
+                                <div class="news-overlay">
+                                    <div class="news-category">{{ $articles[$articleIndex]->category->name }}</div>
+                                    <div class="news-title">
+                                        <h3>{{ $articles[$articleIndex]->title }}</h3>
+                                    </div>
+                                    <div class="news-button">Lees Artikel</div>
+                                    <div class="news-details">
+                                        <div class="news-author">{{ $articles[$articleIndex]->author->firstName }} {{ $articles[$articleIndex]->author->lastName
+                                            }}</div>
+                                        <div class="news-date">{{ $articles[$articleIndex]->created_at->diffForHumans() }}</div>
+                                    </div>
+                                </div>
+                            </div>
+                            </a>
                         </div>
-                        <div class="news-button">Lees Artikel</div>
-                        <div class="news-details">
-                            <div class="news-author">{{ $articles[$articleIndex]->author->firstName }} {{ $articles[$articleIndex]->author->lastName }}</div>
-                            <div class="news-date">{{ $articles[$articleIndex]->created_at->diffForHumans() }}</div>
-                        </div>
-                    </div>
-                    </div>
-                    </div>
-                @endif
+                    @endif 
             @endfor
         </div>
     </div>
     <div class="col-perc-40-gt-30">
 
-    @for($articleIndex = 0; $articleIndex < count($articles); $articleIndex)
-                @if(!$articleIndex & 1)
-                    <div class="news-item">
-                        <div class="box-content">
-                            <h3 class="box-title">{{ $articles[$articleIndex]->title }}</h3>
-                            <p>{{ $articles[$articleIndex]->text }}</p>
-                        </div>
-                        <div class="box-details align-right">
-                            <a href="{{ url('artikel/$articles[$articleIndex]->text') }}">Lees Meer...</a>
-                        </div>
+        @for($articleIndex = 1; $articleIndex < count($articles); $articleIndex++) 
+            @if(!$articleIndex & 1) 
+            <a href="{{ url('/artikels/' . $articles[$articleIndex]->id) }}">
+            <div class="news-item box-medium">
+                <img src="{{ asset('images/hero.jpg') }}" class="news-image" alt="Hero image">
+                <div class="news-overlay">
+                    <div class="news-category">{{ $articles[$articleIndex]->category->name }}</div>
+                    <div class="news-title">
+                        <h3>{{ $articles[$articleIndex]->title }}</h3>
                     </div>
-                @endif
-            @endfor
-        
+                    <div class="news-button">Lees Artikel</div>
+                    <div class="news-details">
+                        <div class="news-author">{{ $articles[$articleIndex]->author->firstName }} {{ $articles[$articleIndex]->author->lastName }}</div>
+                        <div class="news-date">{{ $articles[$articleIndex]->created_at->diffForHumans() }}</div>
+                    </div>
+                </div>
+            </div>
+            </a>
+            @endif 
+        @endfor
+
     </div>
-    </div>--}}
+</div>
 </div>
 
 @endsection
