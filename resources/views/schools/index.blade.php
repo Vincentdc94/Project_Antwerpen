2c65a734c3764c367dfc6372de8c3aa611655dd7
jessekdg <spificator@hotmail.com>
2017-06-02 16:43:35 +0200

    scholen basis frontend behalve media

--- resources/views/schools/index.blade.php
+++ resources/views/schools/index.blade.php
@@ -1,26 +1,32 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>scholen</title>
-</head>
-<body>
+@extends('layouts.app') 
 
-	<h1>Scholen index</h1>
-	<ul>
+@section('header') 
+    @include('partials.header-titled', array('title' => 'Nieuws')) 
+@endsection 
+
+@section("content")
+<div class="container">
 	@foreach($scholen as $school)
-		<li>
-			<b>Naam: </b>{{ $school->name }} <br>
-			<b>Beschrijving: </b>{{ $school->description }}<br>
-			<b>Opleidingen: </b>
-				<ul>
-					@foreach($school->fields as $field)
-						<li>{{ $field->name }}</li>
-					@endforeach
-				</ul>
-				<br>
-		</li>
+		<div class="row">
+			<div class="col-2-gt-30">
+				<a href="{{ url('/scholen/' . $school->id) }}" class="nodecoration">
+					<div class="news-item box-medium">
+						{{--media--}}
+						<div class="news-overlay">
+							<div class="news-title">
+								<h3>{{ $school->name }}</h3>
+							</div>
+							<div class="news-button">Bekijk school</div>
+							<div class="news-details">
+								@foreach($school->fields as $field)
+									{{ $field->name }}
+								@endforeach
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
 	@endforeach
-	</ul>
-
-</body>
-</html>
+</div>
+@endsection
