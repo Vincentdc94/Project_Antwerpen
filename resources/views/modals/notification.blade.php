0e37846fd284651710efd0e04b7925e7ef84e9ee
jessekdg <spificator@hotmail.com>
2017-06-03 14:03:57 +0200

    nu effectief mini aanpassing voor bugfix

--- resources/views/modals/notification.blade.php
+++ resources/views/modals/notification.blade.php
@@ -1,9 +1,8 @@
+ @if(session('message'))
  <div class="notification" id="notification">
-    @if(session('message'))
         <h3 class="notification-title">Notificatie</h3>
         <p class="notification-message" id="notification-message" role="alert">
           {{ session('message') }}
         </p>
-    @endif
  </div>
- 
+ @endif
